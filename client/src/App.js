f6c209d1bcc0754a8453572b93afa0be533d37c4
Toan Le <toanle3322@gmail.com>
2024-04-15 18:52:09 +0700

    Booking hotel Websit

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import './App.css';
+import HomePage from "./pages/HomePage";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+import CreateListing from "./pages/CreateListing";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<HomePage />}/>
+          <Route path="/register" element={<RegisterPage />}/>
+          <Route path="/login" element={<LoginPage />}/>
+          <Route path="/create-listing" element={<CreateListing />}/>
+      </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
